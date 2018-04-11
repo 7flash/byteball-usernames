24ace3ed817c0afe966eb98892a0d2054d3369cd
sevenflash <sevenflash@yandex.ru>
2018-04-12 00:55:40 +0300

    usernames

--- test/test.js
+++ test/test.js
@@ -7,7 +7,7 @@ const exampleUsername = "superuser";
 const examplePerson = "0FNZQSZTPMNZOHYE5R55VIWXF4J63SRG3";
 
 describe("Usernames", () => {
-	describe("should record ownership of usernames", () => {
+	describe("record ownership of usernames", () => {
 		it("should return undefined for available username", () => {
 			expect(usernames.findUsernameOwner(exampleUsername)).to.be.equal(undefined);
 		});
@@ -18,6 +18,38 @@ describe("Usernames", () => {
 
 		it("should return owner of taken username", () => {
 			expect(usernames.findUsernameOwner(exampleUsername)).to.be.equal(examplePerson);
-		})
+		});
+	});
+
+	describe("validate usernames", () => {
+		it("should fail to validate username with less than 5 symbols", () => {
+			expect(usernames.validateUsername("four")).to.be.equal(false);
+		});
+
+		it("should fail to validate numeric username", () => {
+			expect(usernames.validateUsername(55555)).to.be.equal(false);
+		});
+
+		it("should return true for any correct username", () => {
+			expect(usernames.validateUsername("seven")).to.be.equal(true);
+		});
+	});
+
+	describe("determine price", () => {
+		it("should define high price", () => {
+			expect(usernames.highPrice).to.be.a("number");
+		});
+
+		it("should define low price", () => {
+			expect(usernames.lowPrice).to.be.a("number");
+		});
+
+		it("should return high price for usernames shorter than 7 symbols", () => {
+			expect(usernames.getPrice("sixsix")).to.be.equal(usernames.highPrice);
+		});
+
+		it("should return low price for usernames longer than 6 symbols", () => {
+			expect(usernames.getPrice("itseven")).to.be.equal(usernames.lowPrice);
+		});
 	});
 });
--- usernames.js
+++ usernames.js
@@ -1,5 +1,8 @@
 let usernameOwnership = {};
 
+const highPrice = 2000;
+const lowPrice = 1000;
+
 module.exports = {
 	setUsernameOwner(username, person) {
 		usernameOwnership[username] = person;
@@ -7,5 +10,23 @@ module.exports = {
 
 	findUsernameOwner(username) {
 		return usernameOwnership[username];
-	}
+	},
+
+	validateUsername(username) {
+		if(!username.length || username.length < 5)
+			return false;
+
+		return true;
+	},
+
+	getPrice(username) {
+		if(username.length < 7) {
+			return module.exports.highPrice;
+		} else {
+			return module.exports.lowPrice;
+		}
+	},
+
+	highPrice: highPrice,
+	lowPrice: lowPrice
 }
