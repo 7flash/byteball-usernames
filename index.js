574292a769179dc2e3a481bf2d0f1cf12eaa0527
sevenflash <sevenflash@yandex.ru>
2018-04-12 03:53:45 +0300

    bot

--- index.js
+++ index.js
@@ -8,22 +8,17 @@ const usernames = require("./usernames");
 
 const welcomeMessage = "Here you can buy usernames";
 
-eventBus.on("paired", (from) => {
-	reply(from, welcomeMessage);
-});
-
-eventBus.on("text", async (person, text) => {
-	const username = text;
-
-	const owner = await usernames.findUsernameOwner(username);
-
-	if(typeof owner === "undefined") {
-		usernames.setUsernameOwner(username, person);
-		await reply(person, `${username} bought successfully`);
-	} else {
-		await reply(person, `${username} is taken by ${owner}`);
+if (process.env.debug != null) {
+	require("readline").Interface.prototype.question = function(query, cb) {
+		cb("");
 	}
-});
+}
+
+const createPaymentAddress = () => {
+	return new Promise((resolve) => {
+		headlessWallet.issueNextMainAddress(resolve);
+	});
+}
 
 const reply = (recipient, message) => {
 	return new Promise((resolve) => {
@@ -34,3 +29,66 @@ const reply = (recipient, message) => {
 		});
 	});
 }
+
+const executeQuery = async (query, params) => {
+	return new Promise((resolve) => {
+		db.query(query, params, resolve);
+	});
+}
+
+const handleTransaction = async (units) => {
+	const query = `SELECT * FROM outputs WHERE outputs.unit IN(?)`;
+
+	const rows = await executeQuery(query, [units]);
+
+	for(output of rows) {
+		const address = output.address;
+		const amount = output.amount;
+
+		const pendingPayment = usernames.findPendingPaymentByAddress(address);
+
+		if(pendingPayment) {
+			if(amount === pendingPayment.amount) {
+				await usernames.setUsernameOwner(pendingPayment.username, pendingPayment.person);
+
+				await usernames.removePendingPaymentByAddress(address);
+
+				await reply(pendingPayment.person, `${pendingPayment.username} bought successfully`);
+			} else {
+				await reply(pendingPayment.person, `Your payment has been lost, sorry`);
+			}
+		}
+	}
+}
+
+eventBus.on("paired", (from) => {
+	reply(from, welcomeMessage);
+});
+
+eventBus.on("text", async (person, text) => {
+	const username = text;
+
+	const isValid = await usernames.validateUsername(username);
+	if(!isValid) {
+		return reply(person, `${username} is not valid`);
+	}
+
+	const existingOwner = await usernames.findUsernameOwner(username);
+	if(existingOwner) {
+		return reply(person, `${username} is taken by ${existingOwner}`);
+	}
+
+	const pendingPayment = await usernames.findPendingPaymentByUsername(username);
+	if(pendingPayment) {
+		return reply(person, `${username} is already pending for payment`);
+	}
+
+	const address = await createPaymentAddress();
+	const amount = usernames.getPrice(username);
+
+	await usernames.savePendingPayment({ address, amount, person, username });
+
+	await reply(person, `[${username}](byteball:${address}?amount=${amount})`);
+});
+
+eventBus.on("new_my_transactions", handleTransaction);
