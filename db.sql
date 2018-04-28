ccce060fa6efd1d7707c150fe07255d199d9ddd8
sevenflash <sevenflash@yandex.ru>
2018-04-28 22:49:03 +0300

    fix sql

--- db.sql
+++ db.sql
@@ -1,5 +1,5 @@
 CREATE TABLE device_to_wallet (
-    device CHAR(33) UNIQUE NOT NULL PRIMARY KEY,
+    device CHAR(33) NOT NULL PRIMARY KEY,
     wallet CHAR(32) UNIQUE NOT NULL,
     creation_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
@@ -8,7 +8,7 @@ CREATE TABLE wallet_to_reservation (
     wallet CHAR(32) UNIQUE NOT NULL PRIMARY KEY,
     username VARCHAR(320) UNIQUE NOT NULL,
     payment_address CHAR(32) NOT NULL,
-    payment_amount CHAR(32) NOT NULL,
+    payment_amount INT NOT NULL,
     is_confirmed INT NOT NULL DEFAULT 0,
     creation_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
